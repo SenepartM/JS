431257a975ca934f5955965013c6d18e7287ce24
SenepartM <113522450+SenepartM@users.noreply.github.com>
2023-03-08 16:34:33 +0100

    JS9
    ajout d'un <style> css

--- js9.php
+++ js9.php
@@ -4,6 +4,35 @@
     <meta charset="UTF-8">
     <title>Exemple de boutons pour appeler des fonctions</title>
   </head>
+  <style>
+      body {
+        background-color: #F0F0F0;
+        font-family: Arial, sans-serif;
+      }
+
+      .lesdivs {
+        padding: 10px;
+        margin-bottom: 10px;
+        background-color: #FFFFFF;
+        border: 1px solid #CCCCCC;
+        border-radius: 5px;
+      }
+
+      button {
+        background-color: #4CAF50;
+        color: #FFFFFF;
+        border: none;
+        border-radius: 5px;
+        padding: 10px 20px;
+        margin-right: 10px;
+        cursor: pointer;
+        font-size: 16px;
+      }
+
+      button:hover {
+        background-color: #3E8E41;
+      }
+    </style>
   <body>
     <div class="lesdivs"name="lesdivs">Div 1</div>
     <div class="lesdivs"name="lesdivs">Div 2</div>
